b0ff98d28082579656fc45a7272d7b1349e4fdde
Bekatan <bekatans@gmail.com>
2022-11-09 16:15:55 +0900

    Initial

---
+++ lp-spacy.py
@@ -0,0 +1,8 @@
+import spacy
+
+nlp = spacy.load('en_core_web_sm')
+
+doc = nlp(u'Apple is looking at buying U.K. startup for $1 billion')
+
+for token in doc:
+    print(token.text, token.pos_, token.dep_)
