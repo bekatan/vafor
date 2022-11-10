362521df8309c7bf5034c51f91ca2a77601baadf
Bekatan <bekatans@gmail.com>
2022-11-10 14:23:19 +0900

    getTarget implemented. it takes a sentence and returns the desired obj noun

--- lp-spacy.py
+++ lp-spacy.py
@@ -2,7 +2,8 @@ import spacy
 
 nlp = spacy.load('en_core_web_sm')
 
-doc = nlp(u'Apple is looking at buying U.K. startup for $1 billion')
+doc = nlp('can you pass me the book and the smartphone') 
+target = []
 
 for token in doc:
     print(token.text, token.pos_, token.dep_)
---
+++ talker.py
@@ -0,0 +1,41 @@
+import rospy
+import spacy
+from std_msgs.msg import String
+
+SENTENCES = ['can you bring me the cup',
+             'can you pass me the book',
+             'I need that mouse',
+             'I want to eat a carrot',
+             'bring me an orange',
+             'bring that apple to me']
+
+def getTarget(sentence):
+    nlp = spacy.load('en_core_web_sm')
+
+    doc = nlp(sentence)
+
+    target = ""
+
+    for token in doc:
+        if token.dep_ == 'dobj':
+            target = token.text
+            
+    return target
+
+def talker():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        target = getTarget(SENTENCE)
+        rospy.loginfo(target)
+        pub.publish(target)
+        rate.sleep()
+for sentence in SENTENCES:
+    print(getTarget(sentence))
+
+# if __name__ == '__main__':
+#     try:
+#         talker()
+#     except rospy.ROSInterruptException:
+#         pass
