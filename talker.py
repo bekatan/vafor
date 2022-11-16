61445d54342fbe1df97e580ee4e40e058ae84bfe
Bekatan <bekatans@gmail.com>
2022-11-16 17:54:01 +0900

    combined listener and talker

--- talker.py
+++ talker.py
@@ -1,16 +1,42 @@
+#!/usr/bin/env python
+
 import rospy
 import spacy
 from std_msgs.msg import String
+import speech_recognition as sr
 
-SENTENCES = ['can you bring me the cup',
-             'can you pass me the book',
-             'I need that mouse',
-             'I want to eat a carrot',
-             'bring me an orange',
-             'bring that apple to me']
+# SENTENCES = ['can you bring me the cup',
+#              'can you pass me the book',
+#              'I need that mouse',
+#              'I want to eat a carrot',
+#              'bring me an orange',
+#              'bring that apple to me']
 
-def getTarget(sentence):
+
+
+def getTarget():
     nlp = spacy.load('en_core_web_sm')
+    sentence = ""
+
+    try:
+        with sr.Microphone() as source2:
+            r.adjust_for_ambient_noise(source2, duration=0.5)
+            
+            audio2 = r.listen(source2)
+            
+            sentence = r.recognize_google(audio2)
+            sentence = sentence.lower()
+
+            print("Did you say ",sentence)
+            
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+        
+    except sr.UnknownValueError:
+        print("unknown error occurred")
+        
+    if not len(sentence):
+        return ""
 
     doc = nlp(sentence)
 
@@ -25,17 +51,20 @@ def getTarget(sentence):
 def talker():
     pub = rospy.Publisher('chatter', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(10)
+    target = ''
     while not rospy.is_shutdown():
-        target = getTarget(SENTENCES[0])
-        # rospy.loginfo(target)
+        temp = getTarget()
+        if temp != '':
+            target = temp
         pub.publish(target)
         rate.sleep()
-# for sentence in SENTENCES:
-#     print(getTarget(sentence))
 
 if __name__ == '__main__':
+    r = sr.Recognizer()
     try:
         talker()
     except rospy.ROSInterruptException:
         pass
+
+            
